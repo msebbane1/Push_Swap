722c0ea267dc40d6c76e177d761e492a5f39312d
Marie Sebbane <msebbane@c1r2p5.42nice.fr>
2022-03-29 16:32:43 +0200

    push

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: msebbane <msebbane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 12:48:26 by msebbane          #+#    #+#             */
-/*   Updated: 2022/03/29 12:56:47 by msebbane         ###   ########.fr       */
+/*   Updated: 2022/03/29 15:07:48 by msebbane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,107 +22,118 @@
 - Stocker lstnew(argv [1 NULL]) dans ma stack a √
 - [Arguments] Si ils sont deja trie de rien afficher
 */
-int check_double(t_stack *stack_a)
+int	check_double(t_stack *stack_a)
 {
-    t_stack *tmp;
+	t_stack	*tmp;
 
-    while (stack_a) // Parcours ma stack a jusqu'au dernier element [1245] [245] [45] [5]
-    {
-        tmp = stack_a->next; // [245] [45] [5] [NULL]
-        while (tmp)
-        {
+	while (stack_a) // Parcours ma stack a jusqu'au dernier element [1245] [245] [45] [5]
+	{
+    	tmp = stack_a->next; // [245] [45] [5] [NULL]
+		while (tmp)
+		{
             //printf("tmp = %d stack_a = %d\n", tmp->valeur, stack_a->valeur);
-            if (tmp->valeur == stack_a->valeur)
-                return (1);
-            tmp = tmp->next;
-        }
-        stack_a = stack_a->next; // stack a = au dernier element [stack a vers le prochain element]
-    }
-    return (0);
+			if (tmp->valeur == stack_a->valeur)
+				return (1);
+			tmp = tmp->next;
+		}
+		stack_a = stack_a->next; // stack a = au dernier element [stack a vers le prochain element]
+	}
+	return (0);
 }
 
-int check_argv(t_stack **stack_a, char **argv)
+int	check_argv(t_stack **stack_a, char **argv)
 {
-    int     i;
-    int     y;
-    char    **stock_a;
+	int		i;
+	int		y;
+	char	**stock_a;
 
-    i = 1;
-    y = 0;
-    while (argv[i])
-    {
-        stock_a = ft_split(argv[i], ' ');
-        while (stock_a[y])
-        {
-            ft_lstadd_back(stack_a, ft_lstnew(ft_atoi(stock_a[y]))); // arguments stock dans la stack a
-            if (!(ft_isdigit(stock_a[y])) || !(ft_isdigit(argv[i])))
-                return (1);
-            if ((ft_atoi(argv[i]) == 0 && argv[i][0] != '0') ||
-                (ft_atoi(stock_a[y]) == 0 && stock_a[y][0] != '0'))
-                return (1);
-            y++;
-        }
-        i++;
-        y = 0;
-    }
-    return (0);
+	i = 1;
+	y = 0;
+	while (argv[i])
+	{
+		stock_a = ft_split(argv[i], ' ');
+		while (stock_a[y])
+		{
+			ft_lstadd_back(stack_a, ft_lstnew(ft_atoi(stock_a[y]))); // arguments stock dans la stack a
+			if (!(ft_isdigit(stock_a[y])) || !(ft_isdigit(argv[i])))
+				return (1);
+			if ((ft_atoi(argv[i]) == 0 && argv[i][0] != '0') ||
+				(ft_atoi(stock_a[y]) == 0 && stock_a[y][0] != '0'))
+				return (1);
+			y++;
+		}
+		i++;
+		y = 0;
+	}
+	return (0);
 }
 
-int check_sort(t_stack **stack_a)
+int	check_sort(t_stack **stack_a)
 {
-    t_stack *tmp;
+	t_stack	*tmp;
 
-    tmp = *stack_a;
-    while (tmp && tmp->next)
-    {
-        if (tmp->valeur < tmp->next->valeur)
-            tmp = tmp->next;
-        else if (tmp->valeur > tmp->next->valeur)
-            return (0);
-    }
-    return (1);
+	tmp = *stack_a;
+	while (tmp && tmp->next)
+	{
+		if (tmp->valeur < tmp->next->valeur)
+			tmp = tmp->next;
+		else if (tmp->valeur > tmp->next->valeur)
+			return (0);
+	}
+	return (1);
 }
 /*
-void    set_nb_chunk(t_pile *p)
-{   
-    if (p->size_a <= 100)
-        p->nchunk = 5;
-    else if (p->size_a >= 100 && p->size_a <= 500)
-        p->nchunk = 12;
-    else
-        p->nchunk = p->size_a / 15;
-    p->limit = p->size_a / p->nchunk;
-*/
+void    all_chuncks(t_stack **stack_a, int nb_chunck)
+{
+    if (len_stack(stack_a) <= 19)
+        nb_chunck = 1;
+    else if (len_stack(stack_a) >= 20 && len_stack(stack_a) <= 39)
+        nb_chunck = 2;
+    else if (len_stack(stack_a) >= 40 && len_stack(stack_a) <= 59)
+        nb_chunck = 3;
+    else if (len_stack(stack_a) >= 60 && len_stack(stack_a) <= 79)
+        nb_chunck = 4;
+    else if (len_stack(stack_a) >= 80 && len_stack(stack_a) <= 99)
+        nb_chunck = 5;
+}*/
+void	big_stack_chunk(t_stack **stack_a, t_stack **stack_b)
+{
+	int	limit_save;
+	int	nb_chunk;
+
+	if (len_stack(stack_a) > 5 && len_stack(stack_a) <= 100)
+		nb_chunk = 5;
+	else if (len_stack(stack_a) >= 100 && len_stack(stack_a) <= 500)
+		nb_chunk = 11;
+	else
+		nb_chunk = len_stack(stack_a) / 15;
+	limit_save = len_stack(stack_a) / nb_chunk; // ex : 10 nb aleatoires / 5 = 2
+	sort_big_stack_100(stack_a, stack_b, limit_save);
+}
 
 int	main(int argc, char **argv)
 {
 	t_stack	*stack_a;
 	t_stack	*stack_b;
-	int		nb_chunk;
-	int		limit;
 
 	(void) argc;
 	stack_a = new_stack();
 	stack_b = new_stack();
 	if (check_argv(&stack_a, argv) || check_double(stack_a))
 	{
-	    ft_putstr_fd("Error\n", 1);
-	    return (1);
+		ft_putstr_fd("Error\n", 1);
+		return (1);
 	}
 	if (len_stack(&stack_a) == 1 || check_sort(&stack_a) == 1)
-	    return (1);
+		return (1);
 	if (len_stack(&stack_a) == 2)
-	    sa(&stack_a, 2);
+		sa(&stack_a, 2);
 	else if (len_stack(&stack_a) == 3)
-	    sorted_for_3(&stack_a);
+		sorted_for_3(&stack_a);
 	else if (len_stack(&stack_a) <= 5)
-	    sort_small_stack(&stack_a, &stack_b);
-	else if (len_stack(&stack_a) > 5 && len_stack(&stack_a) <= 100)
-	{
-		nb_chunk = 5;
-		limit = len_stack(&stack_a) / nb_chunk; // ex : 20 nb / 5 = 4
-		sort_big_stack_100(&stack_a, &stack_b, limit);
-	}
+		sort_small_stack(&stack_a, &stack_b);
+	else
+		big_stack_chunk(&stack_a, &stack_b);
     //print_stack(stack_a);
 	return (0);
 }
--- srcs/sort_big_stack.c
+++ srcs/sort_big_stack.c
@@ -6,7 +6,7 @@
 /*   By: msebbane <msebbane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 12:01:17 by msebbane          #+#    #+#             */
-/*   Updated: 2022/03/29 12:53:17 by msebbane         ###   ########.fr       */
+/*   Updated: 2022/03/29 16:31:31 by msebbane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,103 +15,146 @@
 /*
 Etape 1 : trouver le plus petit et le mettre en haut de la stack a puis la pull dans la stack b
 Etape 2 : Faire une fonction qui parcours ma stack a len / 2 pour la median en haut de ma median ra en bas = rra
+
 Étape 1 : Analysez la pile A à partir du haut pour confirmer si l'un des numéros du bloc 1 existe à l'intérieur. Appelons ce numéro hold_first.
 Étape 2 : Analysez à nouveau la pile A à partir du bas et voyez si un numéro différent du bloc 1 existe dans cette liste. 
 J'appellerai ce numéro hold_second.
 Étape 3 : Comparez le nombre de mouvements qu'il faudrait pour obtenir hold_first et hold_second au sommet.
 */
-int get_index_min(t_stack **stack)
+int	get_index_up(t_stack **stack_a, int limit)
 {
-    int     i;
-    int     save;
-    int     val;
-    t_stack *tmp;
+	int		i;
+	t_stack	*tmp;
 
-    tmp = *stack;
-    val = tmp->valeur;
-    i = 0;
-    save = i;
-    while (tmp)
-    {
-        if (tmp->valeur < val)
-        {
-            val = tmp->valeur;
-            save = i; // recuperer le i la position
-        }
-        tmp = tmp->next;
-        i++;
-    }
-    return (save);
+	i = 0;
+	tmp = *stack_a;
+	while (tmp)
+	{
+		if (tmp->valeur <= limit)
+		{
+			//printf("i = %d\n", i);
+			return (i);
+		}
+		i++;
+		tmp = tmp->next;
+	}
+	return (i);
 }
-/*
-size_chunk = 0;
-    limit = (p->limit * nb_chunk);
-    while (size_chunk < p->limit && p->size_a != 0)*/
-
-void    hold_chunk(t_stack **stack_a, int limit, int nb_chunk)
+int	get_index_down(t_stack **stack_a, int limit)
 {
-    int chunk_size;
-    int index_min;
+	int		i;
+	int		down;
+	t_stack	*tmp;
 
-    chunk_size = 0;
-    limit = (limit * nb_chunk);
-    while (len_stack(stack_a) != 0 && chunk_size < limit)
-    {
-        index_min = get_index_min(stack_a);
-        search_median(stack_a, index_min);
-        chunk_size++;
-    }
+	i = 0;
+	down = 0;
+	tmp = *stack_a;
+	while (tmp)
+	{
+		if (tmp->valeur <= limit)
+		{
+			//printf("i = %d\n", i);
+			down = i;
+		}
+		i++;
+		tmp = tmp->next;
+	}
+	return (down);
+}
+/* Chercher le minimun dans la stack_a */
+int	get_index_min(t_stack **stack_a)
+{
+	int		i;
+	int		min;
+	int		val;
+	t_stack	*tmp;
+
+	tmp = *stack_a;
+	val = tmp->valeur;
+	i = 0;
+	min = i;
+	while (tmp)
+	{
+		if (tmp->valeur < val)
+		{
+			val = tmp->valeur;
+			min = i; // recuperer le i la position
+		}
+		tmp = tmp->next;
+		i++;
+	}
+	return (min);
+}
+
+void	hold_chunk(t_stack **stack_a, t_stack **stack_b, int limit_save, int nb_chunk)
+{
+	int	chunk_size;
+	int	limit;
+	int	index_up;
+	int	index_down;
+
+	chunk_size = 0;
+	limit = (limit_save * nb_chunk); // ex = 10 nb aleatoire  -- limit = 2 * (1, 2,..,10) = 20
+	//printf("limit = %d\n", limit);
+	while (chunk_size < limit_save && len_stack(stack_a) != 0)
+	{
+		index_up = get_index_up(stack_a, limit);
+		index_down = get_index_down(stack_a, limit);
+		if ((len_stack(stack_a) + 1) - index_down < index_up)
+			search_median(stack_a, index_down);
+		else if ((len_stack(stack_a) + 1) - index_down > index_up)
+			search_median(stack_a, index_up);
+		else
+			search_median(stack_a, index_up);
+		pb(stack_a, stack_b, 2);
+		//print_stack(*stack_a);
+		//printf("up = %d, down = %d\n", chunk_up, chunk_down);
+		chunk_size++;
+	}
+	//printf("chunk_size = %d\n", chunk_size);
 }
 
 /* Cette fonction permet de minimiser le nb de coup, si l'index du minimum est plus petit que 
-la median utilise juste ra, sinon rra
+la median utilise juste le coup ra, sinon rra
 */
-void    search_median(t_stack **stack_a, int index_min)
+void	search_median(t_stack **stack_a, int index_min)
 {
-    int median;
-    int size;
+	int	median;
+	int	size;
 
-    size = len_stack(stack_a);
-    median = (size / 2);
-    if (index_min < median)
-    {
-        while (index_min)
-        {
-            ra(stack_a, 2);
-            index_min--;
-        }
-    }
-    else if (index_min >= median)
-    {
-        while (index_min != size)
-        {
-            rra(stack_a, 2);
-            index_min++;
-        }
-    }
+	size = len_stack(stack_a);
+	median = (size / 2);
+	if (index_min < median)
+	{
+		while (index_min)
+		{
+			ra(stack_a, 2);
+			index_min--;
+		}
+	}
+	else if (index_min >= median)
+	{
+		while (index_min != size)
+		{
+			rra(stack_a, 2);
+			index_min++;
+		}
+	}
 }
 
-/*
-void    count_chunks(t_stack **stack_a, int nb_chunk, int limit)
+void	sort_big_stack_100(t_stack **stack_a, t_stack **stack_b, int limit_save)
 {
-    if (len_stack(stack_a) <= 100)
-        nb_chunk = 5;
-    limit = len_stack(stack_a) / nb_chunk;
-}*/
+	int	nb_chunk;
 
-void    sort_big_stack_100(t_stack **stack_a, t_stack **stack_b, int limit)
-{
-    int nb_chunk;
-
-    nb_chunk = 1;
-    while (len_stack(stack_a) != 0)
-    {
-        hold_chunk(stack_a, limit, nb_chunk);
-        pb(stack_a, stack_b, 2);
-        nb_chunk++;
-    }
-    while (len_stack(stack_b) != 0)
-        pa(stack_a, stack_b, 2);
+	nb_chunk = 1;
+	while (len_stack(stack_a) != 0)
+	{
+		hold_chunk(stack_a, stack_b, limit_save, nb_chunk);
+		printf("nb_chunk = %d\n", nb_chunk);
+		nb_chunk++; // 10 nb = 10 nb
+	}
+	while (len_stack(stack_b) != 0)
+		pa(stack_a, stack_b, 2);
 }
 /*
 void    sort_big_stack_100(t_stack **stack_a, t_stack **stack_b)
@@ -131,20 +174,4 @@ void    sort_big_stack_100(t_stack **stack_a, t_stack **stack_b)
     }
     while (len_stack(stack_b) != 0)
         pa(stack_a, stack_b, 2);
-}*/
-
-
-/*
-void    all_chuncks(t_stack **stack_a, int nb_chunck)
-{
-    if (len_stack(stack_a) <= 19)
-        nb_chunck = 1;
-    else if (len_stack(stack_a) >= 20 && len_stack(stack_a) <= 39)
-        nb_chunck = 2;
-    else if (len_stack(stack_a) >= 40 && len_stack(stack_a) <= 59)
-        nb_chunck = 3;
-    else if (len_stack(stack_a) >= 60 && len_stack(stack_a) <= 79)
-        nb_chunck = 4;
-    else if (len_stack(stack_a) >= 80 && len_stack(stack_a) <= 99)
-        nb_chunck = 5;
 }*/
